65dce275dcb541b32a47a3934f6ac049038b7825
amey1302 <ameykulkarni1322@gmail.com>
2024-04-08 18:33:21 +0530

    Add InvalidTroopTypeException class

---
+++ src/main/java/org/amaap/troopsimulator/service/exception/InvalidTroopTypeException.java
@@ -0,0 +1,7 @@
+package org.amaap.troopsimulator.service.exception;
+
+public class InvalidTroopTypeException extends Exception {
+    public InvalidTroopTypeException(String message) {
+        super(message);
+    }
+}

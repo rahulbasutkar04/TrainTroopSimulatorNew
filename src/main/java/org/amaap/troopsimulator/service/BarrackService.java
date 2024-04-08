27557022fab88c0398cb5e90ba952156cf38196c
amey1302 <ameykulkarni1322@gmail.com>
2024-04-08 23:36:39 +0530

    Update BarrackService class

--- src/main/java/org/amaap/troopsimulator/service/BarrackService.java
+++ src/main/java/org/amaap/troopsimulator/service/BarrackService.java
@@ -1,4 +1,14 @@
 package org.amaap.troopsimulator.service;
 
+import java.util.List;
+import java.util.Queue;
+
 public class BarrackService {
+    public void addTrooper(List<Object> troopers) {
+        // Todo: Need to Implement the service to addTrooper to barrack
+    }
+
+    public void train(Queue<Object> waitingTroopers) {
+        //Todo : Need to implement the train troopers service
+    }
 }

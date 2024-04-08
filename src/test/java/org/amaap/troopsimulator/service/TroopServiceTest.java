36219ea2fa66c5f17a02828fdc3160e84c2b44c9
amey1302 <ameykulkarni1322@gmail.com>
2024-04-08 18:39:14 +0530

    update TroopService test cases

--- src/test/java/org/amaap/troopsimulator/service/TroopServiceTest.java
+++ src/test/java/org/amaap/troopsimulator/service/TroopServiceTest.java
@@ -1,39 +1,61 @@
 package org.amaap.troopsimulator.service;
 
+import org.amaap.troopsimulator.domain.model.Archer;
+import org.amaap.troopsimulator.domain.model.Barbarian;
+import org.amaap.troopsimulator.repository.impl.InMemoryRepository;
 import org.amaap.troopsimulator.service.exception.InvalidTroopCountException;
+import org.amaap.troopsimulator.service.exception.InvalidTroopTypeException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TroopServiceTest {
+    private TroopService troopService;
+    @BeforeEach
+    void setup() {
+      troopService = new TroopService(new InMemoryRepository());
+    }
 
     @Test
     void shouldBeAbleToThrowErrorWhenInvalidNumberCountOfTroopIsGiven() {
         // arrange
-        TroopService troopService = new TroopService();
 
         // act & assert
         assertThrows(InvalidTroopCountException.class, () -> {
             troopService.create(-1, "Barbarian");
         });
     }
+    @Test
+    void shouldBeAbleToCrateTheNumberOfBarbarians() throws InvalidTroopCountException, InvalidTroopTypeException {
+        // arrange
+        int troopCount = 10;
+        String troopType = "Barbarian";
 
-//    @Test
-//
-//    void shouldBeAbleToCrateTheNumberOfBarbarians() throws InvalidTroopCountException {
-//        // arrange
-//        TroopService troopService=new TroopService();
-//         int troopCount=10;
-//         String troopType="Barbarian";
-//        // act
-//        troopService.create(troopCount,troopType);
-//
-//       List<Barbarian> actual= troopService.getBarbarians();
-//
-//        // assert
-//
-//        assertEquals(10,actual.size());
-//    }
+        // act
+        troopService.create(troopCount, troopType);
+        List<Barbarian> actual = troopService.getBarbarians();
 
+        // assert
 
+        assertEquals(10, actual.size());
+    }
+
+    @Test
+    void shouldBeAbleToCrateTheNumberOfArchers() throws InvalidTroopCountException, InvalidTroopTypeException {
+        // arrange
+        int troopCount = 10;
+        String troopType = "Archer";
+
+        // act
+        troopService.create(troopCount, troopType);
+        List<Archer> actual = troopService.getArchers();
+
+        // assert
+
+        assertEquals(10, actual.size());
+    }
 }

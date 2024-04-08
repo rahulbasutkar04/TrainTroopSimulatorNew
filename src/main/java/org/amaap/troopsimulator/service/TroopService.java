a12a35f0f10d809b32e2273a3e8f0b52071771df
amey1302 <ameykulkarni1322@gmail.com>
2024-04-08 18:38:46 +0530

    Update TroopService class

--- src/main/java/org/amaap/troopsimulator/service/TroopService.java
+++ src/main/java/org/amaap/troopsimulator/service/TroopService.java
@@ -1,16 +1,34 @@
 package org.amaap.troopsimulator.service;
 
+import org.amaap.troopsimulator.domain.Troop;
+import org.amaap.troopsimulator.domain.model.Archer;
+import org.amaap.troopsimulator.domain.model.Barbarian;
+import org.amaap.troopsimulator.repository.TroopRepository;
 import org.amaap.troopsimulator.service.exception.InvalidTroopCountException;
+import org.amaap.troopsimulator.service.exception.InvalidTroopTypeException;
+
+import java.util.List;
+
 
 public class TroopService {
-    public static void create(int troopCount, String troopType) throws InvalidTroopCountException {
 
+    private final TroopRepository troopRepository;
 
+    public TroopService(TroopRepository troopRepository) {
+        this.troopRepository = troopRepository;
+    }
+
+    public void create(int troopCount, String troopType) throws InvalidTroopCountException, InvalidTroopTypeException {
         if (troopCount <= 0) throw new InvalidTroopCountException("Troop count can not be:" + troopCount);
+        troopRepository.insert(troopCount, troopType);
+    }
 
-
-
+    public List<Barbarian> getBarbarians() {
+        return troopRepository.getBarbarians();
     }
 
 
+    public List<Archer> getArchers() {
+        return troopRepository.getArchers();
+    }
 }

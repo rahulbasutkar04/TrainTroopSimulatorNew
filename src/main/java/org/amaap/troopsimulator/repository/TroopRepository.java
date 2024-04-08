a70fe36d9816d5cf6fbec524aa2ac5330e257c8e
amey1302 <ameykulkarni1322@gmail.com>
2024-04-08 21:17:50 +0530

    Add TroopRepository

--- src/main/java/org/amaap/troopsimulator/repository/TroopRepository.java
+++ src/main/java/org/amaap/troopsimulator/repository/TroopRepository.java
@@ -2,10 +2,10 @@ package org.amaap.troopsimulator.repository;
 
 import java.util.List;
 
-public  interface TroopRepository  {
-     void insert(int troopCount, String troopType);
+public interface TroopRepository {
+    void insert(int troopCount, String troopType);
 
-     List<Object> getBarbarians();
+    List<Object> getBarbarians();
 
-     List<Object> getArchers();
+    List<Object> getArchers();
 }

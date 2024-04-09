583c7b82f0cd544ee2067bc991d04a029b2605c5
amey1302 <ameykulkarni1322@gmail.com>
2024-04-09 13:48:26 +0530

    Add InMemoryBarrackRepository

---
+++ src/main/java/org/amaap/troopsimulator/repository/impl/InMemoryBarrackRepository.java
@@ -0,0 +1,15 @@
+package org.amaap.troopsimulator.repository.impl;
+
+import org.amaap.troopsimulator.repository.BarrackRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryBarrackRepository implements BarrackRepository {
+    private List<Object> troopersWaitList = new ArrayList<>();
+    @Override
+    public void addTrooperToBarrack(List<Object> troopers) {
+        troopersWaitList.addAll(troopers);
+
+    }
+}

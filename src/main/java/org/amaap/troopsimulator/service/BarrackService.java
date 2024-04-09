c563a382e28b39cc85f846bc5fe92e2541501b51
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-04-09 19:39:13 +0530

    implemented method to train and update in some code

--- src/main/java/org/amaap/troopsimulator/service/BarrackService.java
+++ src/main/java/org/amaap/troopsimulator/service/BarrackService.java
@@ -1,23 +1,32 @@
 package org.amaap.troopsimulator.service;
 
+import org.amaap.troopsimulator.domain.model.Archer;
+import org.amaap.troopsimulator.domain.model.Barbarian;
 import org.amaap.troopsimulator.repository.BarrackRepository;
-import org.amaap.troopsimulator.repository.impl.InMemoryBarrackRepository;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class BarrackService {
     private BarrackRepository barrackRepository;
     private List<Object> trooperQueueInBarrack = new ArrayList<>();
     private TroopService troopService;
+    private Queue<Object> trainedTroops = new LinkedList<>();
+    private final int maxCapacity = 10;
+
+    public Queue<Object> getTrainedTroops() {
+        return trainedTroops;
+    }
 
     public BarrackService(TroopService troopService, BarrackRepository barrackRepository) {
         this.barrackRepository = barrackRepository;
         this.troopService = troopService;
     }
 
-    public List<Object> getTrooperQueueInBarrack() {
-        return trooperQueueInBarrack;
+    public List<Object> getTrooperQueueFromBarrack() {
+        return barrackRepository.getTrooperQueueFromBarrack();
     }
 
     public void addTrooperToBarrack(List<Object> troopers) {
@@ -26,10 +35,36 @@ public class BarrackService {
     }
 
     public void train() {
-        // convert list to queue and clear the list then will get all the troops in the queue, and
-        // then we are passing the troops
-        // batch and adding that data to repo and removing from queue till it gts empty
+        List<Object> troopQueue = getTrooperQueueFromBarrack();
 
+        while (!troopQueue.isEmpty()) {
+            List<Object> batchToTrain = troopQueue.subList(0, Math.min(maxCapacity, troopQueue.size()));
+
+            System.out.println("Training batch...");
+            for (int i = 0; i < batchToTrain.size(); i++) {
+                Object trooper = batchToTrain.get(i);
+                if (trooper instanceof Archer) {
+                    Archer archer = (Archer) trooper;
+                    try {
+                        Thread.sleep(archer.getTrainingTime() * 100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    trainedTroops.add(archer);
+                } else if (trooper instanceof Barbarian) {
+                    Barbarian barbarian = (Barbarian) trooper;
+                    try {
+                        Thread.sleep(barbarian.getTrainingTime() * 100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    trainedTroops.add(barbarian);
+                }
+            }
+            troopQueue.removeAll(batchToTrain);
+        }
     }
+
+    //TODO:now data data is getting stored here in trainedTroop we need to implement an storage in repository->DB
 
 }
